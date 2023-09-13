12cc5aa3ba20e9f02f53d3ad159300b2dda5d77a
Clayton Nagle <c.nagle744@gmail.com>
2023-09-13 11:34:56 +0700

    Added default values

--- main/models.py
+++ main/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
-    amount = models.IntegerField
-    description = models.TextField
+    amount = models.IntegerField(default=0)
+    description = models.TextField(default="No description provided")
