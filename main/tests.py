f08d4bda57bdc29bd24caeaffd5e1237f9cb80e3
Clayton Nagle <c.nagle744@gmail.com>
2023-09-13 11:35:41 +0700

    Added new tests

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from main.models import Item  # Import the Item model from your app
 
 class MainTest(TestCase):
     def test_main_url_is_exist(self):
@@ -8,3 +9,37 @@ class MainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+
+class ItemModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.item = Item.objects.create(name="Sample Item", amount=10, description="This is a test item description.")
+
+    def test_name_field(self):
+        item = Item.objects.get(id=self.item.id)
+        max_length = item._meta.get_field('name').max_length
+        self.assertEqual(max_length, 255)
+    
+    def test_amount_field(self):
+        item = Item.objects.get(id=self.item.id)
+        self.assertEqual(item.amount, 10)
+    
+    def test_description_field(self):
+        item = Item.objects.get(id=self.item.id)
+        self.assertEqual(item.description, "This is a test item description.")
+
+    def test_name_label(self):
+        item = Item.objects.get(id=self.item.id)
+        field = item._meta.get_field('name')
+        self.assertEqual(field.verbose_name, 'name')
+    
+    def test_amount_label(self):
+        item = Item.objects.get(id=self.item.id)
+        field = item._meta.get_field('amount')
+        self.assertEqual(field.verbose_name, 'amount')
+
+    def test_description_label(self):
+        item = Item.objects.get(id=self.item.id)
+        field = item._meta.get_field('description')
+        self.assertEqual(field.verbose_name, 'description')
