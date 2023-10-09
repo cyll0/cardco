599d5d26c0654aa738d2cf22170ccf446e5ccdaa
Clayton Nagle <c.nagle744@gmail.com>
2023-10-09 12:36:43 +0700

    HW6 Done -readme.md

--- main/urls.py
+++ main/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('edit-item/<int:id>', edit_item, name='edit_item'),
     path('delete/<int:id>', delete_item, name='delete_item'),
+    path('get-item/', get_item_json, name='get_item_json'),
+    path('create-item-ajax/', add_item_ajax, name='add_item_ajax')
 ]
--- main/views.py
+++ main/views.py
@@ -12,6 +12,7 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required(login_url='/login')
 def show_main(request):
@@ -111,3 +112,22 @@ def delete_item(request, id):
     item.delete()
     # Return to the main page
     return HttpResponseRedirect(reverse('main:show_main'))
+
+def get_item_json(request):
+    card_item = Item.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize('json', card_item))
+
+@csrf_exempt
+def add_item_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        amount = request.POST.get("amount")
+        description = request.POST.get("description")
+        user = request.user
+
+        new_item = Item(name=name, amount=amount, description=description, user=user)
+        new_item.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
